b6c04238fb0ac198548674cb36857e6ea57fbd69
Petter Nordlander <nordlander.petter@gmail.com>
2022-12-02 07:06:43 +0100

    removed target

--- dec2/target/debug/dec2.d
+++
@@ -1,1 +0,0 @@
-/Users/petter/proj/priv/aoc22/dec2/target/debug/dec2: /Users/petter/proj/priv/aoc22/dec2/input-full.txt /Users/petter/proj/priv/aoc22/dec2/src/main.rs
--- dec2/target/debug/deps/dec2-9518071d8a114bab.d
+++
@@ -1,6 +0,0 @@
-/Users/petter/proj/priv/aoc22/dec2/target/debug/deps/dec2-9518071d8a114bab.rmeta: src/main.rs src/../input-full.txt
-
-/Users/petter/proj/priv/aoc22/dec2/target/debug/deps/dec2-9518071d8a114bab.d: src/main.rs src/../input-full.txt
-
-src/main.rs:
-src/../input-full.txt:
